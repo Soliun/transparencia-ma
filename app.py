c51fc2963483a6129a71298b2ffa6e7bb124d8d4
Josias Silva <jr_solium@hotmail.com>
2019-09-28 11:05:06 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 from flask import Flask
-from flask_restplus import Resource, Api, fields
-from scrapper import despesas_total, despesas_por_funcao
+from flask_restplus import Resource, Api
+from flask_restplus import fields
+from flask_cors import CORS
+
+from scrapper import despesas_total, despesas_por_funcao, despesas_por_orgao
+
 
 app = Flask(__name__)
+CORS(app)
+
+app.config['SWAGGER_UI_DOC_EXPANSION'] = 'list'
+
+
 api = Api(app = app, 
 		  version = "1.0", 
 		  title = "Transparência Maranhão", 
-          description = "Uma API não oficial com os dados sobre as receitas e despesas do Governo do Maranhão")
+          description = "Uma API não oficial com os dados sobre as despesas do Governo do Maranhão")
           
 ns = api.namespace('despesas', description='Dados de despesas')
 
 model = api.model('Dados sobre uma função ou orgão', {
     'codigo': fields.String(description='Código da função ou orgão', example="04"),
     'nome': fields.String(description='Nome da função ou orgão', example="ADMINISTRACAO"),
+    'url_detalhe': fields.String(description='Endereço para mais detalhes', example="http://www.transparencia.ma.gov.br/app/despesas/por-funcao/2016/funcao/04?"),
+    'empenhado': fields.Float(description='Valor empenhado', example=821854500.93),
+    'liquidado': fields.Float(description='Valor liquidado', example=794738131.95),
+    'pago': fields.Float(description='Valor pago', example=775701742.7),
+})
+
+model_credor = api.model('Dados sobre um dado credor', {
+    'cpf/cnpj': fields.String(description='Número do CPF ou do CNPJ do credor', example="04"),
+    'nome': fields.String(description='Nome do credor', example="ADMINISTRACAO"),
     'url_detalhe': fields.String(description='Endereço para mais detalhes', example="http://www.transparencia.ma.gov.br/app/despesas/por-funcao/2016/funcao/04?"),
     'empenhado': fields.Float(description='Valor empenhado', example=821854500.93),
     'liquidado': fields.Float(description='Valor liquidado', example=794738131.95),
@@ -37,7 +55,17 @@ class DespesasPorFuncao(Resource):
     params={ 'ano': 'Ano de referência para as despesas',
     'cod_funcao' : 'Código da função (educação, saúde ...) de referência para as despesas'})
     def get(self, cod_funcao, ano):
-        return despesas_por_funcao(cod_funcao, ano)        
+        return despesas_por_funcao(cod_funcao, ano)
+
+@ns.route('/<string:cod_orgao>/<string:cod_funcao>/<string:ano>')
+class DespesasPorOrgao(Resource):
+
+    @api.marshal_with(model_credor, mask='*')
+    @api.doc(responses={ 200: 'OK', 400: 'Despesas não encontradas' }, 
+    params={ 'ano': 'Ano de referência para as despesas',
+    'cod_orgao' : 'Código do orgão público de referência para as despesas'})
+    def get(self, cod_orgao, cod_funcao, ano):
+        return despesas_por_orgao(cod_orgao, cod_funcao, ano)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- scrapper.py
+++ scrapper.py
@@ -11,6 +11,25 @@ def despesas_por_funcao (cod, ano):
     url = url_base + "/despesas/por-funcao/"+ano+"/funcao/"+cod
     return extrai_despesas (url)
 
+def despesas_por_orgao (orgao, funcao, ano):
+    url = "http://www.transparencia.ma.gov.br/app/despesas/por-funcao/"+ano+"/funcao/"+funcao+"/orgao/"+orgao+"?#lista"
+    response = requests.get(url)
+    page = BS(response.text, 'lxml')
+    table = page.find ('table')
+    rows = table.find_all('tr')
+    despesas = []
+    for row in rows[1:]:
+        cols =row.find_all("td")
+        despesa = {}
+        despesa["nome"] = cols[0].find("a").get_text().strip()
+        despesa["url_detalhe"] = cols[0].find("a").get('href')
+        despesa["cpf/cnpj"] = cols[0].find("small").get_text().strip().replace ("CPF/CNPJ: ","")
+        despesa["empenhado"] =  parse_decimal(cols[1].get_text().strip(), locale='pt_BR')
+        despesa["liquidado"] =  parse_decimal(cols[2].get_text().strip(), locale='pt_BR')
+        despesa["pago"] = parse_decimal (cols[3].get_text().strip(), locale='pt_BR')
+        despesas.append(despesa)
+    return despesas
+
 def extrai_despesas (url):
    response = requests.get(url)
    page = BS(response.text, 'lxml')
